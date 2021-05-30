4ea16cc35b01cd3c446ecc5aa2dad65f0a2dceed
Hawk <cool870427@gmail.com>
2021-05-30 15:16:45 +0800

    del recommender_systems_by_item

--- Code/DataScience/recommender_systems_by_item.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter,defaultdict
-from typing import List,Tuple,Set,Dict
-import numpy as np
-import math
-from Code.DataScience.recommender_systems import users_interests
-
-users_interests_T = np.array(users_interests).T
-print(users_interests_T)
